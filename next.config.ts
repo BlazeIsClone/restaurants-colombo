cdc5ca8148520227bbcb8a155640e972324ac5b0
BlazeIsClone <sandevabeykoon123@gmail.com>
2025-09-14 13:58:48 +0530

    refactor: lazy load map

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "hive-ceylon-xo.sgp1.digitaloceanspaces.com",
-      "images.unsplash.com"
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
     ],
   },
 };
